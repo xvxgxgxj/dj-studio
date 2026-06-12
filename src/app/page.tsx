94e26dab8c700686e433f5127bb3ded329ce61f0
xvxgxgxj <59011613+xvxgxgxj@users.noreply.github.com>
2026-06-13 02:35:53 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,12 @@
 "use client"
 
 import { AppShell } from "@/components/layout/AppShell"
+import { DashboardContent } from "@/components/dashboard/DashboardContent"
 
 export default function Home() {
-  return (
-    <AppShell>
-      <div style={{ color: 'white', padding: '50px' }}>
-        <h1>تجربة: هل يظهر هذا النص؟</h1>
-      </div>
-    </AppShell>
-  )
+  return (
+    <AppShell>
+      <DashboardContent />
+    </AppShell>
+  )
 }
